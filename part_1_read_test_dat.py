c70c3fa241d07aad176a9aa8bd9e01455abf867e
justiny35 <justins.yook@gmail.com>
2019-02-12 14:07:06 -0500

    Phase 1

--- part_1_read_test_dat.py
+++ part_1_read_test_dat.py
@@ -1,7 +1,9 @@
 import cc_dat_utils
+from cc_dat_utils import make_cc_data_from_dat
 
 #Part 1
 input_dat_file = "data/pfgd_test.dat"
 
 #Use cc_dat_utils.make_cc_data_from_dat() to load the file specified by input_dat_file
 #print the resulting data
+print(make_cc_data_from_dat(input_dat_file))
